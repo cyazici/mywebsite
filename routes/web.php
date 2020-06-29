46e2a6646008f45ea45822d363e4187ad414afca
cüneyt yazici <cuuneyt_yazicii@hotmail.com>
2020-06-29 22:27:07 +0300

    son

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function() {
+    return redirect('login');
+});
 
 //Route::group(['middleware'=>['A']],function() {
 Route::get('index','SiteController@index')->name('index');
